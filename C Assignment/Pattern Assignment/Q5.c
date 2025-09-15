d3ba65c7917fe26a0b71d370f5905a737df2e4d8
Ammar-Meman <ammar.meman.cg@gmail.com>
2025-09-15 12:57:23 +0530

    Add C Assignment folder

---
+++ C Assignment/Pattern Assignment/Q5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+int a;
+scanf("%d",&a);
+
+  for(int row=1;row<=a;row++){
+    for(int col=1;col<=a;col++){
+      printf("%d ",col);
+    }
+    printf("\n");
+  }
+}
